76f6af756aae1a0937c5f8e867613176cef70b44
Ronnald Dannyel <ronnalddannyel@gmail.com>
2024-11-30 19:51:41 -0400

    Subindo reposotório

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Rony</title>
+
+      
+
+    </head>
+    <body>
+
+<h1>Bem Vindo</h1>
+<p>Data atual: {{\Carbon\Carbon::now()->format('d/m/Y H:i:s') }} </p>
+    </body>
+</html>
